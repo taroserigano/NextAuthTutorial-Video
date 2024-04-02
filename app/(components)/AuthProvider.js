c01266f1161c8fcfda816821ec64183f19ce18a8
Taro Serigano <taroserigano@gmail.com>
2024-04-02 16:22:40 -0500

    Update AuthProvider.js

--- app/(components)/AuthProvider.js
+++ app/(components)/AuthProvider.js
@@ -4,7 +4,9 @@ import { SessionProvider } from "next-auth/react";
 
 
 // adding the session ability
-// so that you can extract user info from session by like getSession() 
+// so that you can extract user info from session by like:
+// const session = await getServerSession(options);
+
 const AuthProvider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
